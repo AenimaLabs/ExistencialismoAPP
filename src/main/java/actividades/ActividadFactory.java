3852845b10c4f468509f383056cf21da1d8ee9db
AenimaLabs <aenimalabs@gmail.com>
2024-11-08 21:56:11 -0300

    patron factory

--- src/main/java/actividades/ActividadFactory.java
+++ src/main/java/actividades/ActividadFactory.java
@@ -3,19 +3,14 @@ package actividades;
 import interfaz.Actividad;
 
 public class ActividadFactory {
+
     public static Actividad crearActividad(String tipo) {
-        switch (tipo.toLowerCase()) {
-            case "estudiar":
-                return new Estudiar();
 
-            case "trabajar":
-                return new Trabajar();
-
-            case "socializar":
-                return new Socializar();
-
-            default:
-                throw new IllegalArgumentException("Tipo de actividad desconocida");
-        }
+        return switch (tipo.toLowerCase()) {
+            case "estudiar" -> new Estudiar();
+            case "trabajar" -> new Trabajar();
+            case "socializar" -> new Socializar();
+            default -> throw new IllegalArgumentException("Tipo de actividad desconocida");
+        };
     }
 }
